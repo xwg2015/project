6b5ccc7bc4a70252a0b54312050d889dbbf47ff6
xiongwengang <xiongwengang@bilibili.com>
2017-11-27 18:03:26 +0800

    refs 修改sh

--- vue_node_blog/production.sh
+++ vue_node_blog/production.sh
@@ -1,17 +1,17 @@
 # /bin/bash
 
-cp -R ./server/. ../../production/blog_admin_production
+cp -R ./server/. ../../production/admin_production
 
 echo '服务端代码拷贝完成'
 
-cp -R ./dist/css ../../production/blog_admin_production/public
-cp -R ./dist/fonts ../../production/blog_admin_production/public
-cp -R ./dist/img ../../production/blog_admin_production/public
-cp -R ./dist/js ../../production/blog_admin_production/public
+cp -R ./dist/css ../../production/admin_production/public
+cp -R ./dist/fonts ../../production/admin_production/public
+cp -R ./dist/img ../../production/admin_production/public
+cp -R ./dist/js ../../production/admin_production/public
 
 echo '静态资源拷贝完成'
 
-cp -R ./dist/admin.html ../../production/blog_admin_production/views
+cp -R ./dist/admin.html ../../production/admin_production/views
 
 echo '页面拷贝完成'
 
